d059a59bf1cd96efc4533bbdc79f539288d59ed5
nicholaswagner <nicholas@secretsignal.com>
2017-04-14 17:26:48 -0500

    closes #1 bot should know what ENV its running in, and adds a basic bot status command with permission checking

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const botenv = process.env.ENV || "debug" // the bot's current running environment.
 const http = require('http');
 const port = process.env.PORT || 8888; // used for a very simple webserver (keeps heroku from shutting down the bot)
 http.createServer(function (request, response) { response.statusCode = 200; response.end(); }).listen(port);
@@ -19,51 +20,67 @@ const hs = new Hearthstone();
 const TOKEN = process.env.bot_token;
 const SBFVGS_ID = '216034888372060162';
 
+const sbfGuild = {};
+
 let getParams = (content, command) => {
-    let length = command.length + 1;
-    let index = content.search(command);
-    return content.substr(index + length);
+  let length = command.length + 1;
+  let index = content.search(command);
+  return content.substr(index + length);
 }
 
 let getCardCommand = (message) => {
-    let card = getParams(message.content, 'card');
-    hs.card(card, (result) => message.channel.sendMessage(result));
+  let card = getParams(message.content, 'card');
+  hs.card(card, (result) => message.channel.sendMessage(result));
 }
 
 let cardSearchCommand = (message) => {
-    let card = getParams(message.content, 'cardsearch');
-    hs.cardSearch(card, (results) => {
-        results.map((result) => {
-            message.channel.sendMessage(result);
-        });
+  let card = getParams(message.content, 'cardsearch');
+  hs.cardSearch(card, (results) => {
+    results.map((result) => {
+      message.channel.sendMessage(result);
     });
+  });
 }
 
 let xkcdSearchCommand = (message) => {
-    request({ url: 'https://xkcd.com/info.0.json' }, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            let info = JSON.parse(body);
-            message.channel.sendMessage(info.title + '\n' + info.img + '\n' + info.alt);
-        }
-    });
+  request({ url: 'https://xkcd.com/info.0.json' }, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      let info = JSON.parse(body);
+      message.channel.sendMessage(info.title + '\n' + info.img + '\n' + info.alt);
+    }
+  });
+}
+
+let setBotStatusCommand = (message) => {
+  let status = getParams(message.content, 'status');
+  bot.user.setGame(status);
+  console.log("setting bot status to: " + message.content);
 }
 
 let commands = [
-    { name: 'marco', do: message => message.channel.sendMessage('polo') },
-    { name: 'card', do: getCardCommand },
-    { name: 'cardsearch', do: cardSearchCommand },
-    { name: 'xkcd', do: xkcdSearchCommand }
+  { name: 'marco', do: message => message.channel.sendMessage('polo') },
+  { name: 'card', do: getCardCommand },
+  { name: 'cardsearch', do: cardSearchCommand },
+  { name: 'xkcd', do: xkcdSearchCommand },
+  { name: 'status', do: setBotStatusCommand, restricted: true }
 ];
 
 // create an event listener for messages
 bot.on('message', message => {
-    if (message.isMentioned(bot.user)) {
-        commands.map((command) => {
-            if (message.content.includes(command.name)) {
-                command.do(message);
-            }
-        });
-    }
+  if (message.isMentioned(bot.user)) {
+    commands.map((command) => {
+      if (message.content.includes(command.name)) {
+        if (command.restricted) {
+          if (bot.guilds.get(SBFVGS_ID).member(message.author.id).hasPermission("ADMINISTRATOR")) command.do(message)
+          else message.channel.sendMessage("https://media0.giphy.com/media/lAEuCCvZUofAI/giphy.gif");
+        }
+        else {
+          command.do(message);
+        }
+        console.log(message.author + " " + message.author);
+      }
+    });
+  }
 });
 
 let emojis = {};
@@ -72,9 +89,8 @@ let botName = '';
 // the ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted.
 bot.on('ready', () => {
-    emojis = bot.guilds.get(SBFVGS_ID).emojis;
-    bot.user.setGame('your mom');
-    botName = bot.user.username;
+  bot.user.setGame('Latest SBFVGS Podcast');
+  botName = bot.user.username;
 });
 
 // log our bot in
