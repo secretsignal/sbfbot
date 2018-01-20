671fe50b4a21ee7685e4c4fb85ea0b722b7e23ed
Casey Lawrence <casey.j.lawrence@gmail.com>
2018-01-20 12:26:56 -0600

    Add mochaAsyncWrapper helper to properly handle async/await in tests

--- test/helpers/test_helper.js
+++ test/helpers/test_helper.js
@@ -1,6 +1,7 @@
 class TestHelper {
     static getMockMessage() {
         let message = new Object();
+        message.result;
         message.channel = {
             send: () => {},
             sendMessage: () => {} // Depreciated
@@ -9,7 +10,17 @@ class TestHelper {
                 sendMessage: () => {}
         };
         message.reply = () => {};
+        message.testCallback = (r) => { 
+            message.result = r;
+        };
         return message;
+    };
+
+    // https://staxmanade.com/2015/11/testing-asyncronous-code-with-mochajs-and-es7-async-await/
+    static mochaAsyncWrapper(fn) {
+        return (done) => {
+          fn.call().then(done, (err)=>{done(err)});
+        };
     };
 }
 
