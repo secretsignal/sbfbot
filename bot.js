a17c0a28d5ece5bdbe70df4d57785c98a5cb2001
nicholaswagner <nicholas@secretsignal.com>
2018-07-10 08:25:18 -0500

    removes quotes and underscores from reasons when sidebot quotes reaction emoji

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ const botenv = process.env.ENV || "debug" // the bot's current running environme
 const http = require('http');
 const port = process.env.PORT || 8888; // used for a very simple webserver (keeps heroku from shutting down the bot)
 http.createServer(function (request, response) {
-  response.statusCode = 200;
-  response.end();
+	response.statusCode = 200;
+	response.end();
 }).listen(port);
 
 //const Sequelize = require('sequelize')
@@ -26,36 +26,36 @@ const SBFVGS_ID = '216034888372060162';
 // create an event listener for messages
 bot.on('message', message => {
 
-  //if (message.isMentioned(bot.user)) {
-  //  this.commandRouter.route(message);
-  //}
-  if (process.env.botenv === "production") {
-    if (message.content.startsWith("!")) {
-      this.commandRouter.route(message);
-    }
-  } else {
-    if (message.content.startsWith("?")) {
-      this.commandRouter.route(message);
-    }
-  }
+	//if (message.isMentioned(bot.user)) {
+	//  this.commandRouter.route(message);
+	//}
+	if (process.env.botenv === "production") {
+		if (message.content.startsWith("!")) {
+			this.commandRouter.route(message);
+		}
+	} else {
+		if (message.content.startsWith("?")) {
+			this.commandRouter.route(message);
+		}
+	}
 });
 
 // the ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted.
 bot.on('ready', () => {
-  bot.user.setGame('Latest SBFVGS Podcast');
-  this.commandRouter = new commandRouter(bot, SBFVGS_ID);
-  console.log('this bot is now ready.');
+	bot.user.setGame('Latest SBFVGS Podcast');
+	this.commandRouter = new commandRouter(bot, SBFVGS_ID);
+	console.log('this bot is now ready.');
 });
 
 bot.on('guildMemberAdd', member => {
-  member.guild.defaultChannel.send(`Welcome to the server, ${member}!`);
+	member.guild.defaultChannel.send(`Welcome to the server, ${member}!`);
 });
 
 bot.on('messageReactionAdd', (reaction, user) => {
-  if(reaction.emoji.name === "upvote") {
-      reaction.message.channel.send(`<@${reaction.message.author.id}>++ received an upvote from <@${user.id}> for "_${reaction.message.content}_"`);
-  }
+	if (reaction.emoji.name === "upvote") {
+		reaction.message.channel.send(`<@${reaction.message.author.id}>++ received an upvote from <@${user.id}> for ${reaction.message.content}`);
+	}
 });
 
 // log our bot in
